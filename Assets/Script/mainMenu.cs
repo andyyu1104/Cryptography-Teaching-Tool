03c89a9dd61cb6d3503202d918fa5cc77c06b394
andyyu1104 <andyyu1104@gmail.com>
2023-03-14 18:53:59 +0000

    Implemented Start Menu

---
+++ Assets/Script/mainMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+
+    public void PlayGame(){
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+        Debug.Log("Quit!");
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
