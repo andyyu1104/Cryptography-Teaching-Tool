5e828a66b98e5d141161342a26216ac7a52ac27b
andyyu1104 <andyyu1104@gmail.com>
2023-03-23 19:06:24 +0000

    keep volume the same even switch scene

--- Assets/Script/SoundManager.cs
+++ Assets/Script/SoundManager.cs
@@ -16,6 +16,7 @@ public class SoundManager : MonoBehaviour
         }
         else
         {
+            AudioListener.volume = PlayerPrefs.GetFloat("musicVol");
             Load();
         }
     }
