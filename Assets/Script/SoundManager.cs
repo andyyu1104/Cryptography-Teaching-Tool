3a3ab020cd792dbd4b5d5aecf177db1b88f8e49c
andyyu1104 <andyyu1104@gmail.com>
2023-03-23 01:45:12 +0000

    implemented control sound feature by a slider

--- Assets/Script/SoundManager.cs
+++ Assets/Script/SoundManager.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Slider volumeSlider;
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeVolume()
     {
-        
+        AudioListener.volume = volumeSlider.value;
     }
+   
+
 }
